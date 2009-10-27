761be5bd923487e5666773889b95a3aee49cfd56
heinkel <heinkel@87b6d8ef-0863-0410-974a-cfdcb4b1d49e>
2009-10-27 10:32:34 +0000

    added filespyRserve/__init__.py

---
+++ testData/test-script.R
@@ -0,0 +1,26 @@
+# Test file for Ralph with plot returned as raw file
+# 
+# Author: yanabr
+###############################################################################
+
+rm(list=ls())
+graphics.off()
+
+pid <- Sys.getpid()
+
+## some dummy data
+x <- sort(rnorm(100))
+y <- 2*x+rnorm(100,0,0.5)
+
+## model
+model <- lm(y~x)
+
+filename <- paste('plot_',pid,'.png',sep="")
+png(width=480, height=480, file=filename)
+plot(x,y)
+abline(coef(model),col=2,lty=2)
+dev.off()
+
+im <- readBin(filename,"raw", 999999)
+
+result_vector <- list(x,y,coef(model),im)
